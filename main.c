4b0bd1ac3e192e8a8ef668c40e15bf8d9b010757
Ângelo Daumas <42705006+angelo-daumas@users.noreply.github.com>
2021-01-22 19:26:35 -0300

    Escrever inicialização em main.c

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdint.h>
+
 #include "process.h"
 #include "queue.h"
 
@@ -6,15 +9,31 @@ Process processList[2] = {
     {.start=5}
 };
 
-Queue ready_queues[2];
+const uint8_t NUM_PROCESSES = 20;
+const uint8_t NUM_PRIORITIES = 2;
+Queue* ready_queues[NUM_PRIORITIES];
 
-unsigned CPUtime;
-Process currentProcess;
-unsigned timeUsed;
 
-Queue IOqueues[IO_DEVICE_NUMBER];
+unsigned CPUtime = 0;
+Process* currentProcess = NULL;
+
+uint8_t timeUsed = 0;
+const uint8_t MAX_TIMED_USED = 3;
+
+uint8_t PID_GEN = 0;
+
+Queue* IOqueues[IO_DEVICE_NUMBER];
+
+Process* process_table[NUM_PROCESSES] = {0};
 
 int main(void){
     
+    for (int i = 0; i < NUM_PRIORITIES; i++)
+        ready_queues[i] = queue_create(NUM_PROCESSES);
+        
+    
+    for (int i = 0; i < IO_DEVICE_NUMBER; i++)
+        IOqueues[IO_DEVICE_NUMBER] = queue_create(NUM_PROCESSES);
+        
     
 }
