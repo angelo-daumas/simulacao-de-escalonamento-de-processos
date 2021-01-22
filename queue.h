d5abf502264f99316d63027cb43f134b6f24b847
Ângelo Daumas <42705006+angelo-daumas@users.noreply.github.com>
2021-01-22 19:21:45 -0300

    Declarar métodos em queue.h

--- queue.h
+++ queue.h
@@ -1,4 +1,6 @@
- #pragma once
+#pragma once
+
+#include <stdlib.h>
 
  // Defina aqui o tipo dos elementos guardados na fila.
  typedef short TYPE;
@@ -7,4 +9,22 @@ typedef struct Queue {
     TYPE* array;
     int first, last;  // apontam posições no array da fila
     unsigned length, maxlength; 
-} Queue; 
+} Queue;
+
+// Cria uma fila com uma certa capacidade.
+Queue* queue_create(unsigned maxlength);
+
+// Fila está cheia?
+int queue_isfull(Queue* queue);
+
+// Fila está vazia?
+int queue_isempty(Queue* queue);
+
+// Retorna o primeiro elemento da fila.
+TYPE queue_peek(Queue* queue);
+
+// Coloca um elemento no final da fila.
+void queue_push(Queue* queue, TYPE item);
+
+// Tira o elemento do início da fila e o retorna.
+TYPE queue_pop(Queue* queue);
